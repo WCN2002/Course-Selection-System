9e334867597867701b0f1c56aefd8bf0d68f22d0
Kgdrho <124543515+Kgdrho@users.noreply.github.com>
2023-04-13 18:52:48 +0800

    Add files via upload

--- student/searchCourse.php
+++ student/searchCourse.php
@@ -14,129 +14,135 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Course</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h3>Welcome, <?php echo $_SESSION['Username']; ?></h3>
-    <h3><a href = "viewCourse.php">View Course</a></h3>
-    <h3><a href = "logout.php">Sign Out</a></h3>
-    <div class="search-container">
-        <form action="searchCourse.php" method="POST">
-            <input type="text" name="Search" placeholder="Search Course">
-            <button type="submit" name="Submit-search">Search</button>
-        </form>
-    </div>
-    <div class="search-result">
-        <table class="table">
-            <?php
-                if (isset($_POST['Submit-search'])){
-                    $Search = $_POST['Search'];
-                    $Data = "%$Search%";
-                    $sql = "SELECT * FROM courses WHERE courseID LIKE ? OR courseName LIKE ? OR location LIKE ? OR department LIKE ? OR professor LIKE ?";
-                    $stmt = mysqli_prepare($conn, $sql);
-                    mysqli_stmt_bind_param($stmt, "sssss", $Data, $Data, $Data, $Data, $Data);
-                    mysqli_stmt_execute($stmt);
-                    $result = mysqli_stmt_get_result($stmt);
+    <header>
+        <h3 class="welcome-message">Welcome, <?php echo $_SESSION['Username']; ?></h3>
+        <nav class="navigation">
+            <a href = "searchCourse.php">Course Browsing</a>
+            <a href = "viewCourse.php">Profile</a>
+            <a class="btn-logout" href = "logout.php">Sign Out</a>
+        </nav>
+    </header>
+    <section class="page-content">
+        <div class="search-container">
+            <form action="searchCourse.php" method="POST">
+                <input type="text" name="Search" placeholder="Search Course">
+                <button type="submit" name="Submit-search">Search</button>
+            </form>
+        </div>
+        <div class="search-result">
+            <table class="table">
+                <?php
+                    if (isset($_POST['Submit-search'])){
+                        $Search = $_POST['Search'];
+                        $Data = "%$Search%";
+                        $sql = "SELECT * FROM courses WHERE courseID LIKE ? OR courseName LIKE ? OR location LIKE ? OR department LIKE ? OR professor LIKE ? OR day LIKE ? OR time LIKE ?";
+                        $stmt = mysqli_prepare($conn, $sql);
+                        mysqli_stmt_bind_param($stmt, "sssssss", $Data, $Data, $Data, $Data, $Data, $Data, $Data);
+                        mysqli_stmt_execute($stmt);
+                        $result = mysqli_stmt_get_result($stmt);
 
-                    if(mysqli_num_rows($result)>0){
-                        echo '
-                        <thread>
-                        <tr>
-                            <th>Course ID</th>
-                            <th>Course Name</th>
-                            <th>Place</th>
-                            <th>Department</th>
-                            <th>Day</th>
-                            <th>Time</th>
-                            <th>Instructor</th>
-                            <th>Avaliable Seat</th>
-                            <th>Action</th>
-                        </tr>
-                        </thread>
-                        ';
-                        while ($row = mysqli_fetch_assoc($result)){
-                            echo "
-                            <tbody>
+                        if(mysqli_num_rows($result)>0){
+                            echo '
+                            <thead>
                             <tr>
-                                <td>".$row["courseID"]."</td>
-                                <td>".$row["courseName"]."</td>
-                                <td>".$row["location"]."</td>
-                                <td>".$row["department"]."</td>
-                                <td>".$row["day"]."</td>
-                                <td>".$row["time"]."</td>
-                                <td>".$row["professor"]."</td>
-                                <td>".$row["remainSeat"]."/".$row["maxCapacity"]."</td>
-                                <td>
-                                    <a href='courseDetail.php?searchPage=1&courseID=".$row["courseID"]."&outline=".$row["outline"]."'>Detail</a>
-                                    <a href='enroll.php?courseID=".$row["courseID"]."&remainSeat=".$row["remainSeat"]."'>Enroll</a>
-                                <td>
+                                <th>Course ID</th>
+                                <th>Course Name</th>
+                                <th>Place</th>
+                                <th>Department</th>
+                                <th>Day</th>
+                                <th>Time</th>
+                                <th>Instructor</th>
+                                <th>Avaliable Seat</th>
+                                <th>Action</th>
                             </tr>
-                            </tbody>
-                            ";
+                            </thead>
+                            ';
+                            while ($row = mysqli_fetch_assoc($result)){
+                                echo "
+                                <tbody>
+                                <tr>
+                                    <td>".$row["courseID"]."</td>
+                                    <td>".$row["courseName"]."</td>
+                                    <td>".$row["location"]."</td>
+                                    <td>".$row["department"]."</td>
+                                    <td>".$row["day"]."</td>
+                                    <td>".$row["time"]."</td>
+                                    <td>".$row["professor"]."</td>
+                                    <td>".$row["remainSeat"]."/".$row["maxCapacity"]."</td>
+                                    <td>
+                                        <a href='courseDetail.php?searchPage=1&courseID=".$row["courseID"]."&outline=".$row["outline"]."'>Detail</a>
+                                        <a href='enroll.php?courseID=".$row["courseID"]."&remainSeat=".$row["remainSeat"]."'>Enroll</a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                                ";
+                            }
+                            mysqli_stmt_close($stmt);
+                            mysqli_free_result($result);
+                        } else{
+                            echo '<h2>No course found</h2>';
+                            mysqli_stmt_close($stmt);
+                            mysqli_free_result($result);
                         }
-                        mysqli_stmt_close($stmt);
-                        mysqli_free_result($result);
                     } else{
-                        echo '<h2>No course found</h2>';
-                        mysqli_stmt_close($stmt);
-                        mysqli_free_result($result);
-                    }
-                } else{
-                    //show all course for the first time
-                    $sql = "SELECT * FROM courses C ORDER BY C.courseID ASC";
-                    $stmt = mysqli_prepare($conn, $sql);
-                    mysqli_stmt_execute($stmt);
-                    $result = mysqli_stmt_get_result($stmt);
+                        //show all course for the first time
+                        $sql = "SELECT * FROM courses C ORDER BY C.courseID ASC";
+                        $stmt = mysqli_prepare($conn, $sql);
+                        mysqli_stmt_execute($stmt);
+                        $result = mysqli_stmt_get_result($stmt);
 
-                    if(mysqli_num_rows($result)>0){
-                        echo '
-                        <thread>
-                        <tr>
-                            <th>Course ID</th>
-                            <th>Course Name</th>
-                            <th>Place</th>
-                            <th>Department</th>
-                            <th>Day</th>
-                            <th>Time</th>
-                            <th>Instructor</th>
-                            <th>Avaliable Seat</th>
-                            <th>Action</th>
-                        </tr>
-                        </thread>
-                        ';
-                        while ($row = mysqli_fetch_assoc($result)){
-                            echo "
-                            <tbody>
+                        if(mysqli_num_rows($result)>0){
+                            echo '
+                            <thead>
                             <tr>
-                                <td>".$row["courseID"]."</td>
-                                <td>".$row["courseName"]."</td>
-                                <td>".$row["location"]."</td>
-                                <td>".$row["department"]."</td>
-                                <td>".$row["day"]."</td>
-                                <td>".$row["time"]."</td>
-                                <td>".$row["professor"]."</td>
-                                <td>".$row["remainSeat"]."/".$row["maxCapacity"]."</td>
-                                <td>
-                                    <a href='courseDetail.php?searchPage=1&courseID=".$row["courseID"]."&outline=".$row["outline"]."'>Detail</a>
-                                    <a href='enroll.php?courseID=".$row["courseID"]."&remainSeat=".$row["remainSeat"]."'>Enroll</a>
-                                <td>
+                                <th>Course ID</th>
+                                <th>Course Name</th>
+                                <th>Place</th>
+                                <th>Department</th>
+                                <th>Day</th>
+                                <th>Time</th>
+                                <th>Instructor</th>
+                                <th>Avaliable Seat</th>
+                                <th>Action</th>
                             </tr>
-                            </tbody>
-                            ";
+                            </thead>
+                            ';
+                            while ($row = mysqli_fetch_assoc($result)){
+                                echo "
+                                <tbody>
+                                <tr>
+                                    <td>".$row["courseID"]."</td>
+                                    <td>".$row["courseName"]."</td>
+                                    <td>".$row["location"]."</td>
+                                    <td>".$row["department"]."</td>
+                                    <td>".$row["day"]."</td>
+                                    <td>".$row["time"]."</td>
+                                    <td>".$row["professor"]."</td>
+                                    <td>".$row["remainSeat"]."/".$row["maxCapacity"]."</td>
+                                    <td>
+                                        <a href='courseDetail.php?searchPage=1&courseID=".$row["courseID"]."&outline=".$row["outline"]."'>Detail</a>
+                                        <a href='enroll.php?courseID=".$row["courseID"]."&remainSeat=".$row["remainSeat"]."'>Enroll</a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                                ";
+                            }
+                            mysqli_stmt_close($stmt);
+                            mysqli_free_result($result);
+                        } else{
+                            echo '<h2>No course found</h2>';
+                            mysqli_stmt_close($stmt);
+                            mysqli_free_result($result);
                         }
-                        mysqli_stmt_close($stmt);
-                        mysqli_free_result($result);
-                    } else{
-                        echo '<h2>No course found</h2>';
-                        mysqli_stmt_close($stmt);
-                        mysqli_free_result($result);
                     }
-                }
-            
-            ?>
-            
-
-        </table>
-    </div>
+                ?>
+            </table>
+        </div>
+    </section>
+    
 
 </body>
 </html>
